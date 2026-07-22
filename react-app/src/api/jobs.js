2afa006be73db289a305aca7aa9be6ceb4f4c6c0
GKT01 <goktugdal34@gmail.com>
2026-07-22 17:24:17 +0300

    Fixed naming

--- react-app/src/api/jobs.js
+++ react-app/src/api/jobs.js
@@ -1,20 +1,20 @@
 import axios from "./axios";
 
 export const getJobs = async () => {
-    const response = await axios.get("/getAll/");
+    const response = await axios.get("getAll/");
     return response.data;
 };
 
 export const deleteJob = async (id) => {
-    await axios.delete(`/deleteJob/${id}/`);
+    await axios.delete(`deleteJob/${id}/`);
 };
 
 export const updateJob = async (id, jobData) => {
-    const response = await axios.put(`/editJob/${id}/`, jobData);
+    const response = await axios.put(`editJob/${id}/`, jobData);
     return response.data;
 };
 
 export const createJob = async (jobData) => {
-    const response = await axios.post("/createJob/", jobData);
+    const response = await axios.post("createJob/", jobData);
     return response.data;
 };
