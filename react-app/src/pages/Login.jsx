d66b872cc44f9f377bc5887585776cb8a67b77c9
GKT01 <goktugdal34@gmail.com>
2026-07-22 20:40:55 +0300

    Fixed routing

--- react-app/src/pages/Login.jsx
+++ react-app/src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
-import api from "../api";
+import axios from '../api/axios';
 import "../styles/theme.css";
 
 export default function Login() {
